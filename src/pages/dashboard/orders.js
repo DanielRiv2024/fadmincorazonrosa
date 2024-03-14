d1e7fb532c609c9ea4b1c86e16b0ea8d17030c5a
unknown <rivera200224@gmail.com>
2024-03-13 20:33:56 -0600

    Second Test

--- src/pages/dashboard/orders.js
+++ src/pages/dashboard/orders.js
@@ -1,18 +1,17 @@
-import "../../../app/globals.css"
-import OrderHeader from "@/src/components/dashboard/order/OrdersHeader"
-import ListOrders from "@/src/components/dashboard/order/ListOrders"
-import ReportOrder from "@/src/components/dashboard/order/ReportOrder"
-import MenuLeft from "@/src/components/navigation/MenuLeft"
-export default function OrdersPage(){
-    return(
-        <div className="flex flex-row justify-between items-start">
-        <MenuLeft/>
-        <div className="flex flex-col justify-center items-center flex-grow m-5">
-          <OrderHeader/>
-          <ReportOrder/>
-          <ListOrders/>
-        </div>
+import "../../../app/globals.css";
+import OrderHeader from "@/src/components/dashboard/order/OrdersHeader";
+import ListOrders from "@/src/components/dashboard/order/ListOrders";
+import ReportOrder from "@/src/components/dashboard/order/ReportOrder";
+import MenuLeft from "@/src/components/navigation/MenuLeft";
+export default function OrdersPage() {
+  return (
+    <div className="flex flex-row justify-between items-start">
+      <MenuLeft />
+      <div className="flex flex-col justify-center items-center flex-grow m-5">
+        <OrderHeader />
+        <ReportOrder />
+        <ListOrders />
       </div>
-      
-    )
+    </div>
+  );
 }
